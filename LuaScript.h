627fc0285b0007b81ec19d1ec734d1c7e442556e
Elias Daler <eliasdaler@yandex.ru>
2013-10-12 23:29:19 +0400

    Fixed issue #1

--- LuaScript.h
+++ LuaScript.h
@@ -19,44 +19,59 @@ public:
 
     template<typename T>
     T get(const std::string& variableName) {
+      level = 0;
       if(!L) {
         printError(variableName, "Script is not loaded");
         return lua_getdefault<T>();
       }
-      int level = 0;
-      std::string var = "";
-      for(unsigned int i = 0; i < variableName.size(); i++) {
-        if(variableName.at(i) == '.') {
-          if(level == 0) {
-            lua_getglobal(L, var.c_str());
-          } else {
-            lua_getfield(L, -1, var.c_str());
-          }
-          
-          if(lua_isnil(L, -1)) {
-            printError(variableName, var + " is not defined");
-            return lua_getdefault<T>();
-          } else {
-            var = "";
-            level++;
-          }
-        } else {
-          var += variableName.at(i);
-        }
-      }
-      if(level == 0) {
-        lua_getglobal(L, var.c_str());
-      } else {
-        lua_getfield(L, -1, var.c_str());
-      }
-      if(lua_isnil(L, -1)) {
-          printError(variableName, var + " is not defined");
-          return lua_getdefault<T>();
-      }
+      
+      bool success = lua_gettostack<T>(variableName);
 
-      T result = lua_get<T>(variableName);
-      lua_pop(L, level + 1); // pop all existing elements from stack
+      T result;
+      if(success) {
+        result = lua_get<T>(variableName);  
+      } else {
+        result = lua_getdefault<T>();
+      }
+      //std::cout<<"Pushing this many objects from stack:"<<level+1<<std::endl;
+      lua_pop(L, level + 1); // pop all existing elements from stack     
+
       return result;
+    }
+
+    template <typename T>
+    bool lua_gettostack(const std::string& variableName) {
+      std::string var = "";
+        for(unsigned int i = 0; i < variableName.size(); i++) {
+          if(variableName.at(i) == '.') {
+            if(level == 0) {
+              lua_getglobal(L, var.c_str());
+            } else {
+              lua_getfield(L, -1, var.c_str());
+            }
+            
+            if(lua_isnil(L, -1)) {
+              printError(variableName, var + " is not defined");
+              return false;
+            } else {
+              var = "";
+              level++;
+            }
+          } else {
+            var += variableName.at(i);
+          }
+        }
+        if(level == 0) {
+          lua_getglobal(L, var.c_str());
+        } else {
+          lua_getfield(L, -1, var.c_str());
+        }
+        if(lua_isnil(L, -1)) {
+            printError(variableName, var + " is not defined");
+            return false;
+        }
+
+        return true;       
     }
 
     // Generic get
@@ -73,6 +88,7 @@ public:
 private:
     lua_State* L;
     std::string filename;
+    int level;
 };
 
  // Specializations
